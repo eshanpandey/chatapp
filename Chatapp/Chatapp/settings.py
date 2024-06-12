3d87e5bddef66e8e99c66fb7736d1898780e5abe
Eshan Pandey <pandeyeshan13@gmail.com>
2024-06-13 00:17:52 +0530

    Like that

--- Chatapp/Chatapp/settings.py
+++ Chatapp/Chatapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+        'SnapGram.apps.SnapGramConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'daphne',
     'django.contrib.staticfiles',
-        'channels'
-
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ASGI_APPLICATION = 'Chatapp.asgi.application'
